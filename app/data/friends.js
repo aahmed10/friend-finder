1a5d58b7c1cdfab85b6c52509c1e2f9e47188677
Adeel Ahmed <adeelahmed@Adeels-MacBook-Pro.local>
2018-02-10 00:55:20 -0600

    recovered from desktop

---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+//Data 
+//Store all the info when a user has filled out the survey
+//
+
+var friends=[
+    {
+      "name":"James",
+      "photo":"https://i.imgur.com/HxtMPFK.png",
+      "scores":[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+    },
+    {
+      "name":"Karrueche",
+      "photo":"https://i.imgur.com/N35OdMh.jpg",
+      "scores":[
+          2,
+          1,
+          3,
+          4,
+          5,
+          1,
+          2,
+          3,
+          4,
+          5
+        ]
+    } 
+    
+    ];
+    
+    module.exports=friends;
